ef5582f2328dc8184e8150f37727ebbe46542317
lewisian8787 <lewisian8787@gmail.com>
2026-02-18 17:05:30 -0500

    updating dist

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(express.json())
 // app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
